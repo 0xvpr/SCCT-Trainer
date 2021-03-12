2e1f901cef5ab04feb38d9da4b4993072b73eb84
malikrb <malikrb@live.com>
2021-03-12 11:49:26 -0600

    Default python script message

--- pushmaster.py
+++ pushmaster.py
@@ -8,6 +8,6 @@ if len(sys.argv) > 1:
     message = sys.argv[1]
 
 if __name__ == '__main__':
-    os.system("git add .")
+    os.system(f"git add .")
     os.system(f"git commit -m \"{message}\"")
-    os.system("git add .")
+    os.system(f"git push origin master")
