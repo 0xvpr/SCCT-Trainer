0c714ed1b5200bf3e5abc330fdf45b985313a473
Malik R. Booker <malikrb@live.com>
2021-05-14 07:42:45 -0500

    formatting

--- c/include/offsets.h
+++ c/include/offsets.h
@@ -19,7 +19,7 @@ unsigned int offsets_rapid_fire_base         = 0x00178FA2;
 
 // Multi Level Pointers
 unsigned int offsets_entity_list_pointers[2] = { 0x78, 0x5E4 };
-unsigned int offsets_weapon_pointers[3]      = { 0x84, 0x6E0, 0x434 };
+unsigned int offsets_weapon_pointers[3]     = { 0x84, 0x6E0, 0x434 };
 
 // Length of Multi-level Pointers
 size_t offsets_entity_list_pointers_size = sizeof(offsets_entity_list_pointers) / sizeof(int);
