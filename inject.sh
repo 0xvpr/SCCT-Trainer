7c3ae15c608807873f8708b4964181d35524e47e
0xvpr <malikrb@live.com>
2024-02-26 00:27:43 -0500

    inject script updated

--- inject.sh
+++ inject.sh
@@ -2,15 +2,12 @@
 
 # Creator: VPR
 # Created: December 14th, 2021
-# Updated: December 24th, 2021
+# Updated: February 26th, 2024
 
 # This script is intended to work with Pidjeon for a seamless injection process.
 
 TARGET="splintercell3.exe"      # Change as necessary
 PAYLOAD="lib/sp3.dll"           # Change as necessary
 
-function inject {
-    vpr-pidjeon-x86.exe "${TARGET}" "${PAYLOAD}" -i ManualMap
-}
-
-inject
+! [[ $(which vpr-pidjeon) ]] && echo 'vpr-pidjeon' not found in path && exit 1
+vpr-pidjeon "${TARGET}" "${PAYLOAD}" -i ManualMap
